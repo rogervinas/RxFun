f782919acdd7aaf428520bf2ff49e3dc80626aa9
Roger Viñas Alcon <roger.vinas@scmspain.com>
2017-05-28 22:22:50 +0200

    Add all sync tests

--- src/test/java/com/rogervinas/rxfun/ConcatObservablesTest.java
+++ src/test/java/com/rogervinas/rxfun/ConcatObservablesTest.java
@@ -1,29 +1,73 @@
 package com.rogervinas.rxfun;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import rx.Observable;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@RunWith(Parameterized.class)
 public class ConcatObservablesTest {
 
-    private List<Integer> valuesOneToTen = Observable.range(1, 10).toList().toBlocking().first();
+    private final List<Integer> valuesOneToTen = Observable.range(1, 10).toList().toBlocking().first();
 
-    private static final Duration oneSecond = Duration.ofSeconds(1);
-    private static final Duration twoSeconds = Duration.ofSeconds(2);
-    private static final Duration fourSeconds = Duration.ofSeconds(4);
+    private static final Duration duration1 = Duration.ofMillis(500);
+    private static final Duration duration2 = duration1.multipliedBy(2);
+    private static final Duration duration4 = duration1.multipliedBy(4);
 
-    @Test
-    public void sync_timeout_completed_concat_with_sync_timeout_completed() {
-        List<Integer> values = ConcatObservables.concat(
-            Observables.syncObservable(1, 5, oneSecond, fourSeconds, true),
-            twoSeconds,
-            Observables.syncObservable(6, 10, oneSecond, fourSeconds, true)
-        ).toList().toBlocking().first();
-        assertThat(values).isEqualTo(valuesOneToTen);
+    private static final Observable<Integer> observable1_sync_timeout_completed = Observables.syncObservable(1, 5, duration1, duration4, true);
+    private static final Observable<Integer> observable1_sync_timeout_no_completed = Observables.syncObservable(1, 5, duration1, duration4, false);
+    private static final Observable<Integer> observable1_sync_no_timeout_completed = Observables.syncObservable(1, 5, duration1, duration1, true);
+    private static final Observable<Integer> observable1_sync_no_timeout_no_completed = Observables.syncObservable(1, 5, duration1, duration1, false);
+
+    private static final Observable<Integer> observable2_sync_timeout_completed = Observables.syncObservable(6, 10, duration1, duration4, true);
+    private static final Observable<Integer> observable2_sync_timeout_no_completed = Observables.syncObservable(6, 10, duration1, duration4, false);
+    private static final Observable<Integer> observable2_sync_no_timeout_completed = Observables.syncObservable(6, 10, duration1, duration1, true);
+    private static final Observable<Integer> observable2_sync_no_timeout_no_completed = Observables.syncObservable(6, 10, duration1, duration1, false);
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> parameters() {
+        Collection<Object> observables1 = Arrays.asList(
+            observable1_sync_timeout_completed,
+            observable1_sync_timeout_no_completed,
+            observable1_sync_no_timeout_completed,
+            observable1_sync_no_timeout_no_completed
+        );
+        Collection<Object> observables2 = Arrays.asList(
+            observable2_sync_timeout_completed,
+            observable2_sync_timeout_no_completed,
+            observable2_sync_no_timeout_completed,
+            observable2_sync_no_timeout_no_completed
+        );
+        Collection<Object[]> observables = new ArrayList<>();
+        for(Object observable1 : observables1) {
+            for(Object observable2 : observables2) {
+                observables.add(new Object[] { observable1, observable2});
+            }
+        }
+        return observables;
     }
 
+    @Parameterized.Parameter(0)
+    public Observable<Integer> observable1;
+
+    @Parameterized.Parameter(1)
+    public Observable<Integer> observable2;
+
+    @Test(timeout = 15_000)
+    public void test() {
+        List<Integer> values = ConcatObservables.concat(
+            observable1,
+            duration2,
+            observable2
+        ).take(10).toList().toBlocking().first();
+        assertThat(values).isEqualTo(valuesOneToTen);
+    }
 }
